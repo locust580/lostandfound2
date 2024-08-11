57277fb2b0090970dadf794a8c6be79fa3f46ced
locust580 <91351495+locust580@users.noreply.github.com>
2024-08-10 23:18:03 -0500

    slow steps towards img upload

--- views/submit.js
+++ views/submit.js
@@ -21,16 +21,14 @@ document.getElementById('submitButton').addEventListener("click", () => {
     //assigns the first submitted file from array inputElem 
     let file = inputElem.files[0];
 
-    /* no more blobbing no need for this 
+    //slices image into a useable array blobl blobl bloblb
+    let blob = file.slice(0, file.size, "image/jpeg");
+    console.log(blob)
 
-        //slices image into a useable array blobl blobl bloblb
-        let blob = file.slice(0, file.size, "image/jpeg");
-
-    */
 
     //SIKE we just remake file but new name no blob
     //thanks github: https://stackoverflow.com/questions/51782270/change-name-of-uploaded-file-on-client
-    newFile = new File([file], `${postid}_post.jpeg`, { type: "image/jpeg" });
+    newFile = new File([blob], `${postid}_post.jpeg`, { type: "image/jpeg" });
 
     //creates new FormData object 
     let formData = new FormData();
@@ -47,23 +45,23 @@ document.getElementById('submitButton').addEventListener("click", () => {
         body: formData,
     })
         .then(res => res.text())
-        .then(loadPosts());
+        // .then(loadPosts());
 });
 
 
-function loadPosts(){
-  fetch('/upload')
-  .then(res => res.json())
-  .then((x) => {
-    for(y = 0; y < x[0].length; y++){
-      const newimg = document.createElement('img');
-      newimg.setAttribute(
-        'src',
-        'https://storage.googleapis.com/lost-found-storage/' + x[0][y].id
-      );
-      newimg.setAttribute["height", 50];
-      newimg.setAttribute["width", 50]
-      document.getElementById('images').appendChild(newimg);
-    }
-  });
-}
+// function loadPosts(){
+//   fetch('/upload')
+//   .then(res => res.json())
+//   .then((x) => {
+//     for(y = 0; y < x[0].length; y++){
+//       const newimg = document.createElement('img');
+//       newimg.setAttribute(
+//         'src',
+//         'https://storage.googleapis.com/lost-found-storage/' + x[0][y].id
+//       );
+//       newimg.setAttribute["height", 50];
+//       newimg.setAttribute["width", 50]
+//       document.getElementById('images').appendChild(newimg);
+//     }
+//   });
+// }
